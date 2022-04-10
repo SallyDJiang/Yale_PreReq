56bd0f61782a57842780fc06042f4cab278ab177
Sally Jiang <46103394+SallyDJiang@users.noreply.github.com>
2022-04-10 02:22:03 -0400

    updated setup file

--- PreReq_Project/setup.py
+++ PreReq_Project/setup.py
@@ -4,16 +4,3 @@ import pandas as pd
 import os.path 
 import sys 
 
-
-# check the dataset is inside files 
-
-datapath = ''
-if os.path.isdir(datapath): 
-    required_files = ["Pre_Req.csv"]
-    for file in required_files:
-        filedir = os.path.isfile(datapath+file)
-        if not filedir:
-            print(file + " is not in Folder")
-            sys.exit()
-        else:
-            continue
